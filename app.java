075b36af726d5fe20717ddccac3ecb5e85bbcc8c
root <root@ip-172-31-13-55.ap-south-1.compute.internal>
2025-07-31 02:37:55 +0000

    first ever code included

---
+++ app.java
@@ -0,0 +1,9 @@
+public class Greeting {
+    static void hello() {
+        System.out.println("Hello.. Happy learning!");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+}
