e579f826c8ac3653e04053548b9dfd8a40a3308c
Sharathreddy22 <154256017+Sharathreddy22@users.noreply.github.com>
2025-08-07 16:47:18 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,16 +1,19 @@
 class Main {
   public static void main(String[] args) {
 
-    int n = 10, firstTerm = 0, secondTerm = 1;
-    System.out.println("Fibonacci Series till " + n + " terms:");
+    String str = "Radar", reverseStr = "";
+    
+    int strLength = str.length();
 
-    for (int i = 1; i <= n; ++i) {
-      System.out.print(firstTerm + ", ");
+    for (int i = (strLength - 1); i >=0; --i) {
+      reverseStr = reverseStr + str.charAt(i);
+    }
 
-      // compute the next term
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+      System.out.println(str + " is not a Palindrome String.");
     }
   }
 }
